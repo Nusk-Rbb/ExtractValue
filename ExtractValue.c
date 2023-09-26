b69e1c1060cfc6fa145f83692976a0e8b8082ce2
NuskRbb <dpanda.kky@gmail.com>
2023-09-27 00:47:01 +0900

    change: 関数の上にあった詳細をプロトタイプ宣言の上に移動しました

--- ExtractValue.c
+++ ExtractValue.c
@@ -2,15 +2,26 @@
 #include <string.h>
 
 // プロトタイプ宣言
-char* find(char* text, char target, int target_num);
-int lencount(char* text, char target);
-double getvalue(char* text, int len);
- 
+
 /// @brief targetで検索文字を指定すると検索させた文字列のアドレスが返ってきます
 /// @param text 検索させたい文字列の先頭アドレス
 /// @param target 検索させる文字
 /// @param target_num 複数の文字がある場合のいくつ目の文字を指定
 /// @return 検索した文字のアドレス
+char* find(char* text, char target, int target_num);
+
+/// @brief 検索する文字列までカウントします
+/// @param text 検索させたい文字列の先頭アドレス
+/// @param target 検索したい文字
+/// @return カウントした数字
+int lencount(char* text, char target);
+
+/// @brief 抽出したい文字列を数値に変換する関数です
+/// @param text 抽出させる文字列の先頭アドレス
+/// @param len 抽出したい文字列の長さ
+/// @return 抽出した数値
+double getvalue(char* text, int len);
+ 
 char* find(char *text, char target, int target_num){
     size_t len = strlen(text);
     int count = 0;
@@ -26,10 +37,7 @@ char* find(char *text, char target, int target_num){
 
     return NULL;
 }
-/// @brief 検索する文字列までカウントします
-/// @param text 検索させたい文字列の先頭アドレス
-/// @param target 検索したい文字
-/// @return カウントした数字
+
 int lencount(char* text, char target){
     size_t len = strlen(text);
     text += 1;
@@ -43,10 +51,6 @@ int lencount(char* text, char target){
     return 0;
 }
 
-/// @brief 抽出したい文字列を数値に変換する関数です
-/// @param text 抽出させる文字列の先頭アドレス
-/// @param len 抽出したい文字列の長さ
-/// @return 抽出した数値
 double getvalue(char* text, int len){
     double value;
     char char_value[len];
